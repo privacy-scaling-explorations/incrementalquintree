0cd4f9a627425185088e9fe1dc2b1df82196589f
Koh Wei Jie <contact@kohweijie.com>
2021-05-09 12:23:22 +0100

    Do not populate filledSubtrees in the contract constructor as that is redundant

--- contracts/MultiIncrementalQuinTree.sol
+++ contracts/MultiIncrementalQuinTree.sol
@@ -84,7 +84,7 @@ contract MultiIncrementalQuinTree is Ownable, Hasher {
 
         for (uint8 i = 0; i < _treeLevels; i++) {
             for (uint8 j = 0; j < LEAVES_PER_NODE; j ++) {
-                filledSubtrees[i][j] = currentZero;
+                //filledSubtrees[i][j] = currentZero;
                 originalFilledSubtrees[i][j] = currentZero;
                 temp[j] = currentZero;
             }
